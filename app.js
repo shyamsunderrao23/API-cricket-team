349fa05f59c15e73a1189a4856a56b67a357ef2f
shyamsunderrao23 <tankalashyamsunderrao@gmail.com>
2023-05-29 09:18:42 +0000

    API-solution

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+
+const dbPath = path.join(__dirname, "cricketTeam.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+const convertDbObjectToResponseObject = (dbObject) => {
+  return {
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+    jerseyNumber: dbObject.jersey_number,
+    role: dbObject.role,
+  };
+};
+
+app.get(" /players/", async (request, response) => {
+  const getallPalyers = `
+    SELECT * FROM cricket_team 
+    `;
+  const playerArray = await db.all(getallPalyers);
+  response.send(
+    playerArray.map((eachPlayer) => convertDbObjectToResponseObject(eachPlayer))
+  );
+});
+module.exports = app;
